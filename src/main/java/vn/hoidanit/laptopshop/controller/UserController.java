c8b33b5874f34a2ce255b097890b147da619f0ee
hiepnt8703 <hiepnt8703@gmail.com>
2024-10-05 16:03:06 +0700

    update_model

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,8 +16,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("hiep", test);
+        model.addAttribute("tuanhiepit", "From Controller");
         return "hello";
     }
 }
