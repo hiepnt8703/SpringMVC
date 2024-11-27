4e6f4f28c2920ce9204188d6ed363ac2610f4b59
hiepnt8703 <hiepnt8703@gmail.com>
2024-11-27 23:04:43 +0700

    update_detail

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -1,14 +1,29 @@
 package vn.hoidanit.laptopshop.controller.client;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+
 @Controller
 public class ItemController {
+
+    private final ProductService productService;
+    
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public String getProductDetail(Model model, @PathVariable long id) {
+        Optional<Product> product = this.productService.getProductId(id);
+        model.addAttribute("product", product.get());
+        model.addAttribute("id", id);
         return "client/product/detail";
     }
 
